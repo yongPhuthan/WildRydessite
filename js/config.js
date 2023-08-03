e1769b77866aa6e11246e12c7e13586f0bf690fb
yongPhuthan <86427357+yongPhuthan@users.noreply.github.com>
2023-08-03 18:41:04 +0700

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_l4FjuY2qk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'g7ptvu0plakan7fs20srm8rdl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
